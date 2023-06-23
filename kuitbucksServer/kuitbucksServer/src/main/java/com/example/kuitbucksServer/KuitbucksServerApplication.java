b54beeee28c59cfd8343d7ccb05858de63c4fb8a
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-06-23 10:58:56 +0900

    feat: default spring

---
+++ kuitbucksServer/kuitbucksServer/src/main/java/com/example/kuitbucksServer/KuitbucksServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kuitbucksServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuitbucksServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuitbucksServerApplication.class, args);
+	}
+
+}
---
+++ kuitbucksServer/kuitbucksServer/src/test/java/com/example/kuitbucksServer/KuitbucksServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kuitbucksServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuitbucksServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
